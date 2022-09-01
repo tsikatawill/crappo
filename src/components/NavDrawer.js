9813c7653f914be8d2dbdd437eadbf17d2dda10d
William M. Tsikata <tsikatawill@gmail.com>
2022-09-01 08:21:35 +0000

    create navigation drawer component

---
+++ src/components/NavDrawer.js
@@ -0,0 +1,34 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { NavbarDrawerV } from "../variants";
+import { Navlink } from "./Navbar";
+
+export default function NavDrawer({ showMenu }) {
+  return (
+    <AnimatePresence>
+      {showMenu && (
+        <motion.div
+          variants={NavbarDrawerV}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="nav-drawer flex flex-col md:hidden justify-center top-0 left-0 w-full h-screen bg-accent absolute gap-8 items-center text-2xl"
+        >
+          <Navlink text="Products" />
+          <Navlink text="Features" />
+          <Navlink text="About" />
+          <Navlink text="Contact" />
+
+          <div className="cta flex items-center gap-5">
+            <Navlink text="Login" />
+            <span className="text-slate-500">|</span>
+            <button className="btn">Register</button>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+NavDrawer.defaultProps = {
+  showMenu: false,
+};
