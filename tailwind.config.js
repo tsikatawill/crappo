42db75b84941df82fe5b1710d61e0309a9d9ef4a
William M. Tsikata <tsikatawill@gmail.com>
2022-09-01 08:15:44 +0000

    add default color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0D0D2B",
+        accent: "#252540",
+      },
+    },
   },
   plugins: [],
-}
+};
