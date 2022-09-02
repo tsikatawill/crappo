6e1d3557b7ae517cdeeeef9a5d040b3a2a5dc187
William M. Tsikata <tsikatawill@gmail.com>
2022-09-02 18:49:29 +0000

    format as per prettier config

--- src/variants.js
+++ src/variants.js
@@ -1,40 +1,44 @@
 export const NavbarDrawerV = {
-  hidden: { x: "100vw" },
-  show: { x: 0, transition: { duration: 1 } },
+  hidden: { x: '100vw' },
+  show: { x: 0, transition: { duration: 0.5 } },
+  exit: { x: '100vw', transition: { duration: 0.5 } }
 };
 
 export const AnimateDownV = (duration = 0.8, yOffset = -100) => {
   return {
     initial: { y: yOffset, opacity: 0 },
-    animate: { y: 0, opacity: 1, transition: { duration } },
+    animate: { y: 0, opacity: 1, transition: { duration } }
   };
 };
 
 export const AnimateUpV = {
   initial: { y: 100 },
-  animate: { y: 0, transition: { duration: 1 } },
+  animate: { y: 0, transition: { duration: 1 } }
 };
 
 export const AnimateRightV = {
   initial: { x: -100, opacity: 0 },
-  animate: { x: 0, opacity: 1, transition: { duration: 1 } },
+  animate: { x: 0, opacity: 1, transition: { duration: 1 } }
 };
 
 export const AnimateLeftV = {
   initial: { x: 100, opacity: 0 },
-  animate: { x: 0, opacity: 1, transition: { duration: 1 } },
+  animate: { x: 0, opacity: 1, transition: { duration: 1 } }
 };
 
 export const ScaleUpV = {
   initial: { scale: 0.7, opacity: 0 },
-  animate: { scale: 1, opacity: 1, transition: { duration: 1 } },
+  animate: { scale: 1, opacity: 1, transition: { duration: 1 } }
 };
 
 export const StaggerContainer = {
-  initial: {},
-  animate: {
-    transition: {
-      staggerChildren: 0.3,
-    },
+  initial: {
+    y: 100
   },
+  animate: {
+    y: 0,
+    transition: {
+      staggerChildren: 0.3
+    }
+  }
 };
