bb96ef5e706940eacf510d9b5321621eaadd0542
William M. Tsikata <tsikatawill@gmail.com>
2022-09-02 18:48:01 +0000

    prevent scroll when menu is displayed

--- src/components/NavDrawer.jsx
+++ src/components/NavDrawer.jsx
@@ -1,9 +1,19 @@
 import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 import { NavbarDrawerV } from '../variants';
 import { Navlink } from './Navbar';
 
 export default function NavDrawer({ showMenu, onClose }) {
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (showMenu) {
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'auto';
+    }
+  }, [showMenu]);
+
   return (
     <AnimatePresence>
       {showMenu && (
@@ -11,8 +21,8 @@ export default function NavDrawer({ showMenu, onClose }) {
           variants={NavbarDrawerV}
           initial="hidden"
           animate="show"
-          exit="hidden"
-          className="nav-drawer flex flex-col md:hidden justify-center top-0 left-0 w-full h-screen bg-accent absolute gap-8 items-center text-2xl"
+          exit="exit"
+          className="nav-drawer flex flex-col md:hidden justify-center top-0 left-0 w-full h-screen bg-accent fixed gap-8 items-center z-40. text-2xl"
           onClick={onClose}
         >
           <Navlink text="Products" />
