e21eb85d73a63b668b9fbfcf1e71dc772505d7b4
William M. Tsikata <tsikatawill@gmail.com>
2022-09-02 14:07:02 +0000

    import all new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Navbar from "./components/Navbar";
+import CryptoCurrencySection from './components/CryptoCurrencySection';
+import DemographicsSection from './components/DemographicsSection';
+import HashCalculatorSection from './components/HashCalculatorSection';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import WhySection from './components/WhySection';
 
 export default function App() {
   return (
     <>
-    <Navbar />
+      <header>
+        <Navbar />
+        <Hero />
+        <DemographicsSection />
+        <WhySection />
+        <HashCalculatorSection />
+        <CryptoCurrencySection />
+        <div className="space h-96"></div>
+      </header>
     </>
   );
 }
-
